796ffbdc61d049310bd06cac56f6bfa8206a97d9
Max Garner <49308171+KalamariKing@users.noreply.github.com>
2021-02-09 10:11:41 -0500

    Update FieldDayTeacher.cpp
    Updated

--- FieldDayTeacher.cpp
+++ FieldDayTeacher.cpp
@@ -1,54 +1,44 @@
-// FieldDayTeacher.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// FieldDayTeacher.cpp : This file contains the 'main' function. Program execution begins and ends here.
 
 #include "Headers.h"
 
-sf::TcpSocket serverLink;
-sf::Packet packet;
-
-string input;
-
+char action [2] = "0";
 string id = "Griffin";
-string action;
-int value;
+int value, grade5Score, grade6Score, grade7Score, grade8Score;
 
-sf::Uint32 grade5Score;
-sf::Uint32 grade6Score;
-sf::Uint32 grade7Score;
-sf::Uint32 grade8Score;
+sf::Packet packet;
+sf::TcpSocket socket;
 
 int main()
 {
-    cout << "Press 1 and then ENTER to get a task" << endl;
-    cout << "Press 2 and then ENTER to get the current score" << endl;
+  // Prompt User - Loop until *action* is either "1" or "2"
+  // (strcmp() compares strings; 0 means they are the same)
+  while (strcmp(action, "1") !=0 && strcmp(action, "2") !=0)
+  {
+    cout << endl << "Press 1 and then ENTER to get a task" << endl << "Press 2 and then ENTER to get the current score" << endl;
+    cin >> action;
+  }
+  
+  // Connect to server
+  socket.connect("10.0.0.209", 53000);
 
-    cin >> input;
+  // Attempt to send request
+  packet << id << action << value;
+  if (socket.send(packet, 2) != sf::Socket::Done)
+  {
+    cout << "Error sending request" << endl;
+  }
 
-    if (input == "1")
-    {
-        serverLink.connect("10.0.0.209", 53000);
+  // If the intent was to check the scores, then recieve
+  if (strcmp(action, "2") == 0)
+  {
+    socket.receive(packet);
 
-        action = "task";
+    packet >> grade5Score >> grade6Score >> grade7Score >> grade8Score;
 
-        packet << id << action << value;
+    cout << grade5Score << grade6Score << grade7Score << grade8Score << endl;
 
-        serverLink.send(packet);
-    }
-
-    if (input == "2")
-    {
-        serverLink.connect("10.0.0.209", 53000);
-
-        action = "score";
-
-        packet << id << action << value;
-
-        serverLink.send(packet);
-
-        serverLink.receive(packet);
-
-        packet >> grade5Score >> grade6Score >> grade7Score >> grade8Score;
-
-        cout << grade5Score << grade6Score << grade7Score << grade8Score;
-    }
+    // Should output the same thing as the above two lines? If so, we can save variable space... if not, delete this
+    cout << packet << endl;
+  }
 }
